60ef7c9f6cfe6e61538cfc2d45a95ea74a6aff7f
naveendennis <naveendennis@gmail.com>
2017-06-08 22:03:07 -0400

    Refactoring and Fixes for reconnect

--- facebook_requests.py
+++ facebook_requests.py
@@ -2,42 +2,45 @@ import requests
 import configparser
 import json
 import string
+import pathlib
+import os
+
+config = configparser.ConfigParser()
+config.read('properties/config.ini')
 
 
 def load():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    config = config['facebook']
+    global config
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--url',
                         type=str,
-                        default=config['url'],
+                        default=config['facebook']['url'],
                         help='THe url that is present in the Facebook Graph API tool')
     parser.add_argument('--access_token',
                         type=str,
-                        default=config['access_token'],
+                        default=config['facebook']['access_token'],
                         help='The access token used by the application')
     parser.add_argument('--method',
                         type=str,
-                        default=config['method'],
+                        default=config['facebook']['method'],
                         help='Type of method used by request - get/post')
     parser.add_argument('--domain',
                         type=str,
-                        default=config['domain'],
+                        default=config['facebook']['domain'],
                         help='The starting portion of the "url" argument. Do not change this.')
     args = parser.parse_args()
     # access_token = get_access_token(args, config)
-    return args, config
+    return args
 
 
 def get_new_access_token(config):
-    refresh_token_url = config['domain'] + \
+    refresh_token_url = config['facebook']['domain'] + \
                         'oauth/access_token?' + \
                         'grant_type=fb_exchange_token&' + \
-                        'client_id=' + config['client_id'] + '&' + \
-                        'client_secret=' + config['client_secret'] + '&' + \
-                        'fb_exchange_token=' + config['access_token']
+                        'client_id=' + config['facebook']['client_id'] + '&' + \
+                        'client_secret=' + config['facebook']['client_secret'] + '&' + \
+                        'fb_exchange_token=' + config['facebook']['access_token']
     new_access_token = requests.get(refresh_token_url)
     return json.loads(new_access_token.text)['access_token']
 
@@ -52,30 +55,59 @@ def extract_date(datestring):
     return date_mentioned
 
 
+def file_exists(path):
+    path = pathlib.Path(path)
+    return path.is_file()
+
+
 def get_response(url):
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    config.set('facebook', 'access_token', get_new_access_token(config['facebook']))
-    with open('config.ini', 'w') as f:
-        config.write(f)
-    response = requests.get(url + '&access_token=' + config['facebook']['access_token'])
+    global config
+    access_config = configparser.ConfigParser()
+    page_id = config['facebook']['url'].split('?')[0].split('/')[0]
+    access_token_fn = 'properties/access_token'+page_id+'.ini'
+
+    if file_exists(access_token_fn):
+        access_config.read(access_token_fn)
+        config['facebook']['access_token'] = access_config['facebook']['access_token']
+    else:
+        access_config.add_section('facebook')
+        access_config.set('facebook', 'access_token', config['facebook']['access_token'])
+
+    config.set('facebook', 'access_token', get_new_access_token(config))
+    with open(access_token_fn, 'w') as f:
+        access_config.write(f)
+    response = requests.get(url + '&access_token=' + access_config['facebook']['access_token'])
     return response
 
 
-def get_all_posts(args, config, start_date, end_date):
+def reconnect(url):
+    global config
+    counter = 0
+    response = get_response(url)
+    while response.status_code != 200:
+        counter += 1
+        response = get_response(url)
+        if counter == int(config['facebook']['reconnect']):
+            return None
+    return response
+
+
+def get_all_posts(args, start_date, end_date):
+    global config
     url = construct_url(domain=args.domain,
                         url=args.url,
                         access_token=args.access_token)
     response = get_response(url)
     start_pointer = json.loads(response.text)
-    config['access_token'] = args.access_token
-    return get_them_all(args, config, start_date, end_date, start_pointer)
+    config['facebook']['access_token'] = args.access_token
+    return get_them_all(args, start_date, end_date, start_pointer)
 
 
-def is_in_topic(message, config):
+def is_in_topic(message):
+    global config
     result = False
     translator = str.maketrans(' ', ' ', string.punctuation)
-    considered_tokens = config['searchTokens'].split(' ')
+    considered_tokens = config['facebook']['searchTokens'].split(' ')
     tokens_in_each_status = message.strip().translate(translator).lower().split(' ')
     for each_considered_token in considered_tokens:
         if each_considered_token in tokens_in_each_status:
@@ -84,8 +116,9 @@ def is_in_topic(message, config):
     return result
 
 
-def get_them_all(args, config, start_date, end_date, start_pointer, has_comment=True):
+def get_them_all(args, start_date, end_date, start_pointer, has_comment=True):
 
+    global config
     all_posts = list()
     while True:
         try:
@@ -98,10 +131,13 @@ def get_them_all(args, config, start_date, end_date, start_pointer, has_comment=
             date_mentioned = extract_date(date_mentioned)
             if start_date < date_mentioned < end_date:
                 if has_comment:
-                    if not is_in_topic(each_status['message'], config):
+                    try:
+                        if not is_in_topic(each_status['message']):
+                            continue
+                    except KeyError:
                         continue
 
-                    each_status['comments'] = get_them_all(args, config, start_date, end_date,
+                    each_status['comments'] = get_them_all(args, start_date, end_date,
                                                            each_status['comments'],
                                                            has_comment=False)
 
@@ -115,9 +151,9 @@ def get_them_all(args, config, start_date, end_date, start_pointer, has_comment=
             url = start_pointer['paging']['next']
         except KeyError:
             break
-        response = get_response(url)
-        while response.status_code != 200:
-            response = get_response(url)
+        response = reconnect(url)
+        if response is None:
+            break
         start_pointer = json.loads(response.text)
 
     return all_posts
@@ -136,13 +172,21 @@ def construct_json_for_page(all_posts, args):
         json.dump(result, f)
 
 
+def make_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
 def main():
-    args, config = load()
-    start_date = extract_date(config['start_date'])
-    end_date = extract_date(config['end_date'])
-    all_posts = get_all_posts(args, config, start_date, end_date)
+    args = load()
+    start_date = extract_date(config['facebook']['start_date'])
+    end_date = extract_date(config['facebook']['end_date'])
+    all_posts = get_all_posts(args, start_date, end_date)
     construct_json_for_page(all_posts, args)
 
 
 if __name__ == '__main__':
+    import timeit
+    start_time = timeit.default_timer()
     main()
+    elapsed_time = timeit.default_timer() - start_time
+    print('elapsed time :', elapsed_time)
